fb4340da9e4e39666df74c6bb46fa8056b65b362
Alexander Enrique Toscano Ricardo <kikret@gmail.com>
2020-10-15 14:49:25 -0500

    Nuevo cambio
    cambio del mensaje

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hola Soy el estudiante');
+    res.send('Hola Soy el docente');
 })
 
 app.listen(port, () => {
